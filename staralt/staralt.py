ac2ebfd618b8fc32f87bc6918d9ad759af695ad0
Simon Walker <s.r.walker101@googlemail.com>
2014-05-01 00:18:42 +0100

    Adds basic required parameters testing

--- staralt/staralt.py
+++ staralt/staralt.py
@@ -1,5 +1,23 @@
 class InsufficientParameters(RuntimeError): pass
 
 class StarAlt(object):
+    DEFAULT_MOON_DISTANCE = True
+    DEFAULT_MIN_ELEVATION = 30
+
+    REQUIRED_PARAMS = ['mode', 'date', 'coordinates',
+            'moon_distance', 'min_elevation']
+
+    def __init__(self):
+        self.moon_distance = self.DEFAULT_MOON_DISTANCE
+        self.min_elevation = self.DEFAULT_MIN_ELEVATION
+        self.mode = None
+        self.date = None
+        self.coordinates = []
+
     def save_image(self, filename):
-        raise InsufficientParameters
+        if self.insufficient_parameters():
+            raise InsufficientParameters
+
+    def insufficient_parameters(self):
+        return all(getattr(self, param) is not None
+                for param in self.REQUIRED_PARAMS)
--- testing/test_staralt_class.py
+++ testing/test_staralt_class.py
@@ -1,8 +1,33 @@
 from staralt.staralt import StarAlt, InsufficientParameters
 import pytest
+import mock
+from datetime import date
 
-def test_fail_with_invalid_parameters():
+@mock.patch.object(StarAlt, 'insufficient_parameters', return_value=True)
+def test_fail_with_invalid_parameters(mock_staralt):
     s = StarAlt()
 
     with pytest.raises(InsufficientParameters) as err:
         s.save_image("image.gif")
+
+@mock.patch('requests.get')
+@mock.patch.object(StarAlt, 'insufficient_parameters', return_value=False)
+def test_success_with_valid_parameters(mock_staralt, mock_get):
+    s = StarAlt()
+    s.save_image("image.gif")
+
+def test_insufficient_parameters():
+    s = StarAlt()
+    assert s.insufficient_parameters() == False
+
+@mock.patch('staralt.coordinate.Coordinate')
+def test_sufficient_parameters(mock_coordinate):
+    s = StarAlt()
+    s.mode = 'starobs'
+    s.date = date.today()
+    s.coordinates = [mock_coordinate, ]
+
+    assert s.moon_distance == True
+    assert s.min_elevation == 30
+
+    assert s.insufficient_parameters() == True
